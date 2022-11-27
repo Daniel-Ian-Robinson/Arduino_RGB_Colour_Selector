7b368415bd3c61b5e8e390c2c6c28e629ea3110a
Daniel-Ian-Robinson <daniel.robinson.21.dr@gmail.com>
2022-11-28 11:48:50 +1300

    Commented rgb_colour_selector.ino

--- rgb_colour_selector/rgb_colour_selector.ino
+++ rgb_colour_selector/rgb_colour_selector.ino
@@ -6,8 +6,16 @@ const int RED_OUT_PIN = 3;
 const int GREEN_OUT_PIN = 5;
 const int BLUE_OUT_PIN = 6;
 
+/**
+ * The minimum potentiometer value required to activate the LED. Use a sensible value in the range [0, 1023]. This
+ * threshold exists because potentiometers rarely hit zero but we need to accept zero values so we can send zero values
+ * to the LED.
+ */
 const int MIN_POT_VAL = 15;
 
+/**
+ * Sets up the potentiometer input pins and the LED output pins.
+ */
 void setup() {
     pinMode(RED_POT_PIN, INPUT);
     pinMode(GREEN_POT_PIN, INPUT);
@@ -16,16 +24,20 @@ void setup() {
     pinMode(RED_OUT_PIN, OUTPUT);
     pinMode(GREEN_OUT_PIN, OUTPUT);
     pinMode(BLUE_OUT_PIN, OUTPUT);
-
-    Serial.begin(9600);
 }
 
+/**
+ * Reads the potentiometer values and updates the LED's colour accordingly.
+ */
 void loop() {
     int redPotVal = analogRead(RED_POT_PIN);
     int greenPotVal = analogRead(GREEN_POT_PIN);
     int bluePotVal = analogRead(BLUE_POT_PIN);
 
-    // Since the potentiometers rarely hit zero, we will have a cap so the lkight doesnt flicker.
+    /*
+     *  Floor the potentiometer values to zero if they are below the threshold. This stops the LED flickering when the
+     *  potentiometer is at its minimum value (which may be above zero).
+     */
     if (redPotVal < MIN_POT_VAL) {
         redPotVal = 0;
     }
@@ -38,10 +50,12 @@ void loop() {
         bluePotVal = 0;
     }
 
+    // Calculate the red, green, and blue values for the LED from the potentiometers' input.
     int redVal = map(redPotVal, 0, 1023, 0, 255);
     int greenVal = map(greenPotVal, 0, 1023, 0, 255);
     int blueVal = map(bluePotVal, 0, 1023, 0, 255);
 
+    // Set the LED's colour.
     analogWrite(RED_OUT_PIN, redVal);
     analogWrite(GREEN_OUT_PIN, greenVal);
     analogWrite(BLUE_OUT_PIN, blueVal);
