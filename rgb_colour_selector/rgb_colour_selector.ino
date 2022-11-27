8122fe96e688feb582fcdb6e51a82cce9ff197f1
Daniel-Ian-Robinson <daniel.robinson.21.dr@gmail.com>
2022-11-28 11:19:30 +1300

    Created schematic, made min. pot. val.

--- rgb_colour_selector/rgb_colour_selector.ino
+++ rgb_colour_selector/rgb_colour_selector.ino
@@ -6,6 +6,8 @@ const int RED_OUT_PIN = 3;
 const int GREEN_OUT_PIN = 5;
 const int BLUE_OUT_PIN = 6;
 
+const int MIN_POT_VAL = 15;
+
 void setup() {
     pinMode(RED_POT_PIN, INPUT);
     pinMode(GREEN_POT_PIN, INPUT);
@@ -24,23 +26,23 @@ void loop() {
     int bluePotVal = analogRead(BLUE_POT_PIN);
 
     // Since the potentiometers rarely hit zero, we will have a cap so the lkight doesnt flicker.
-    if (redPotVal < 20) {
+    if (redPotVal < MIN_POT_VAL) {
         redPotVal = 0;
     }
 
-    if (greenPotVal < 20) {
+    if (greenPotVal < MIN_POT_VAL) {
         greenPotVal = 0;
     }
 
-    if (bluePotVal < 20) {
+    if (bluePotVal < MIN_POT_VAL) {
         bluePotVal = 0;
     }
 
-  int redVal = map(redPotVal, 0, 1023, 0, 255);
-  int greenVal = map(greenPotVal, 0, 1023, 0, 255);
-  int blueVal = map(bluePotVal, 0, 1023, 0, 255);
+    int redVal = map(redPotVal, 0, 1023, 0, 255);
+    int greenVal = map(greenPotVal, 0, 1023, 0, 255);
+    int blueVal = map(bluePotVal, 0, 1023, 0, 255);
 
-  analogWrite(RED_OUT_PIN, redVal);
-  analogWrite(GREEN_OUT_PIN, greenVal);
-  analogWrite(BLUE_OUT_PIN, blueVal);
+    analogWrite(RED_OUT_PIN, redVal);
+    analogWrite(GREEN_OUT_PIN, greenVal);
+    analogWrite(BLUE_OUT_PIN, blueVal);
 }
